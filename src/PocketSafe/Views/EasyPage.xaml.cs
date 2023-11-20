ef6632bb01521ee5836c09c462a2a610233c96cc
David King Roderos <iamdavidkingroderos@gmail.com>
2023-11-20 23:05:20 +0800

    Added EasyViewModel

--- src/PocketSafe/Views/EasyPage.xaml.cs
+++ src/PocketSafe/Views/EasyPage.xaml.cs
@@ -1,9 +1,13 @@
+using PocketSafe.ViewModels;
+
 namespace PocketSafe.Views;
 
 public partial class EasyPage : ContentPage
 {
-	public EasyPage()
+	public EasyPage(EasyViewModel viewModel)
 	{
 		InitializeComponent();
+
+		BindingContext = viewModel;
 	}
 }
