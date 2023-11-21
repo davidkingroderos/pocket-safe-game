859fb95269173ba3b0df21780f668516b0b6c776
David King Roderos <iamdavidkingroderos@gmail.com>
2023-11-21 23:18:05 +0800

    Implemented medium difficulty

--- src/PocketSafe/ViewModels/MediumViewModel.cs
+++ src/PocketSafe/ViewModels/MediumViewModel.cs
@@ -10,7 +10,16 @@ namespace PocketSafe.ViewModels
 {
     public partial class MediumViewModel : BaseViewModel
     {
-        protected override bool HasWon => throw new NotImplementedException();
+        protected override bool HasWon
+        {
+            get
+            {
+                if (!R0C0_IsClickable && !R0C1_IsClickable && !R1C0_IsClickable && !R1C1_IsClickable && 
+                    !R2C0_IsClickable && !R2C1_IsClickable && !R3C0_IsClickable && !R3C1_IsClickable)
+                    return true;
+                else return false;
+            }
+        }
         public MediumViewModel()
         {
             Title = "Medium";
@@ -36,7 +45,6 @@ namespace PocketSafe.ViewModels
         [ObservableProperty]
         private bool r3C1_IsClickable = true;
 
-
         protected override void ResetButtons()
         {
             R0C0_IsClickable = true;
@@ -54,49 +62,209 @@ namespace PocketSafe.ViewModels
         [RelayCommand]
         private void R0C0_Click()
         {
+            if (!HasStarted)
+            {
+                HasStarted = true;
+            }
+            else if (PrevNumber != (ButtonNumbers[0] - 1) && !(PrevNumber == SafeSize && ButtonNumbers[0] == 1))
+            {
+                ResetButtons();
+            }
 
+            R0C0_IsClickable = false;
+
+            if (HasWon)
+            {
+                Shell.Current.CurrentPage.DisplayAlert("Unlocked!", 
+                    "You've unlocked the safe! Try hard difficulty next!", "Restart");
+
+                ResetButtons();
+                InitializeButtons();
+            }
+
+            PrevNumber = ButtonNumbers[0];
         }
 
         [RelayCommand]
         private void R0C1_Click()
         {
+            if (!HasStarted)
+            {
+                HasStarted = true;
+            }
+            else if (PrevNumber != (ButtonNumbers[1] - 1) && !(PrevNumber == SafeSize && ButtonNumbers[1] == 1))
+            {
+                ResetButtons();
+            }
 
+            R0C1_IsClickable = false;
+
+            if (HasWon)
+            {
+                Shell.Current.CurrentPage.DisplayAlert("Unlocked!", 
+                    "You've unlocked the safe! Try hard difficulty next!", "Restart");
+
+                ResetButtons();
+                InitializeButtons();
+            }
+
+            PrevNumber = ButtonNumbers[1];
         }
 
         [RelayCommand]
         private void R1C0_Click()
         {
+            if (!HasStarted)
+            {
+                HasStarted = true;
+            }
+            else if (PrevNumber != (ButtonNumbers[2] - 1) && !(PrevNumber == SafeSize && ButtonNumbers[2] == 1))
+            {
+                ResetButtons();
+            }
 
+            R1C0_IsClickable = false;
+
+            if (HasWon)
+            {
+                Shell.Current.CurrentPage.DisplayAlert("Unlocked!", 
+                    "You've unlocked the safe! Try hard difficulty next!", "Restart");
+
+                ResetButtons();
+                InitializeButtons();
+            }
+
+            PrevNumber = ButtonNumbers[2];
         }
 
         [RelayCommand]
         private void R1C1_Click()
         {
+            if (!HasStarted)
+            {
+                HasStarted = true;
+            }
+            else if (PrevNumber != (ButtonNumbers[3] - 1) && !(PrevNumber == SafeSize && ButtonNumbers[3] == 1))
+            {
+                ResetButtons();
+            }
 
+            R1C1_IsClickable = false;
+
+            if (HasWon)
+            {
+                Shell.Current.CurrentPage.DisplayAlert("Unlocked!", 
+                    "You've unlocked the safe! Try hard difficulty next!", "Restart");
+
+                ResetButtons();
+                InitializeButtons();
+            }
+
+            PrevNumber = ButtonNumbers[3];
         }
 
         [RelayCommand]
         private void R2C0_Click()
         {
+            if (!HasStarted)
+            {
+                HasStarted = true;
+            }
+            else if (PrevNumber != (ButtonNumbers[4] - 1) && !(PrevNumber == SafeSize && ButtonNumbers[4] == 1))
+            {
+                ResetButtons();
+            }
 
+            R2C0_IsClickable = false;
+
+            if (HasWon)
+            {
+                Shell.Current.CurrentPage.DisplayAlert("Unlocked!",
+                    "You've unlocked the safe! Try hard difficulty next!", "Restart");
+
+                ResetButtons();
+                InitializeButtons();
+            }
+
+            PrevNumber = ButtonNumbers[4];
         }
 
         [RelayCommand]
         private void R2C1_Click()
         {
+            if (!HasStarted)
+            {
+                HasStarted = true;
+            }
+            else if (PrevNumber != (ButtonNumbers[5] - 1) && !(PrevNumber == SafeSize && ButtonNumbers[5] == 1))
+            {
+                ResetButtons();
+            }
 
+            R2C1_IsClickable = false;
+
+            if (HasWon)
+            {
+                Shell.Current.CurrentPage.DisplayAlert("Unlocked!",
+                    "You've unlocked the safe! Try hard difficulty next!", "Restart");
+
+                ResetButtons();
+                InitializeButtons();
+            }
+
+            PrevNumber = ButtonNumbers[5];
         }
 
         [RelayCommand]
         private void R3C0_Click()
         {
+            if (!HasStarted)
+            {
+                HasStarted = true;
+            }
+            else if (PrevNumber != (ButtonNumbers[6] - 1) && !(PrevNumber == SafeSize && ButtonNumbers[6] == 1))
+            {
+                ResetButtons();
+            }
 
+            R3C0_IsClickable = false;
+
+            if (HasWon)
+            {
+                Shell.Current.CurrentPage.DisplayAlert("Unlocked!",
+                    "You've unlocked the safe! Try hard difficulty next!", "Restart");
+
+                ResetButtons();
+                InitializeButtons();
+            }
+
+            PrevNumber = ButtonNumbers[6];
         }
 
         [RelayCommand]
         private void R3C1_Click()
         {
+            if (!HasStarted)
+            {
+                HasStarted = true;
+            }
+            else if (PrevNumber != (ButtonNumbers[7] - 1) && !(PrevNumber == SafeSize && ButtonNumbers[7] == 1))
+            {
+                ResetButtons();
+            }
 
+            R3C1_IsClickable = false;
+
+            if (HasWon)
+            {
+                Shell.Current.CurrentPage.DisplayAlert("Unlocked!",
+                    "You've unlocked the safe! Try hard difficulty next!", "Restart");
+
+                ResetButtons();
+                InitializeButtons();
+            }
+
+            PrevNumber = ButtonNumbers[7];
         }
 
         #endregion
